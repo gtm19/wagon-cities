97bd3bc1269813da9c5a2f1d7340520e5735bf03
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-05-08 19:33:25 +0100

    create setCities action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 // TODO: add and export your own actions
+import cities from '../data/cities';
+
+const setCities = () => {
+
+  // In real life this would probably do some sort of API call
+
+  return {
+    type: "SET_CITIES",
+    payload: cities
+  };
+};
+
+export { setCities };
