c07e1ae5ede5e78867ceef14c2e97a60b3685903
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-05-08 17:42:55 +0100

    switched city_list to use defaultProps

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import CityList from "../containers/city_list";
 
-const cities = [
-  { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
-  { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
-  { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
-];
-
 const App = () => {
   return (
     <div className="app container-fluid">
-      <CityList cities={cities} />
+      <CityList />
       <div className="m-3 col-md-8"> {/* TODO: replace this with ActiveCity */}
         Some more content
       </div>
--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 class CityList extends Component {
+  static defaultProps = {
+    cities: [
+      { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
+      { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
+      { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
+    ]
+  };
+
   constructor(props) {
     super(props);
 
